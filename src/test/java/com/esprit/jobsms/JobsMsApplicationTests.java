1e4bc65ca88bd941e96ed73a8abd75c8edb4f623
Amdouni-Dev <amdouni.mouna@esprit.tn>
2023-09-19 21:48:00 +0100

    SimpleCrudJob

---
+++ src/test/java/com/esprit/jobsms/JobsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.jobsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
